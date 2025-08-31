6f6515bdbecfa74998d6e254e9dda66823033f75
mauricio17092003 <mauricio17092003@gmail.com>
2025-08-31 10:51:57 -0300

    Último Commit

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ document.addEventListener("DOMContentLoaded", () => {
     message.style.color = "green";
     form.reset();
   });
+
+  const faders = document.querySelectorAll(".fade-in");
+  const appearOptions = { threshold: 0.3 };
+
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add("appear");
+      observer.unobserve(entry.target);
+    }, appearOptions);
+
+    faders.forEach((fader) => {
+      appearOnScroll.observe(fader);
+    });
+  });
 });
